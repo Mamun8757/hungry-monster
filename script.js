21db529ccb0e1ce7f7b163e8870d67f62cc78a40
Mamun8757 <almamun8757@gmail.com>
2021-02-08 19:54:02 +0600

    food details problem fix

--- script.js
+++ script.js
@@ -1,66 +1,55 @@
-function findMeal() {
-    const searchBtn = document.getElementById("food");
-    document.getElementById("displayMeals").innerHTML = "";
-    document.getElementById("description").innerHTML = "";
-    fetch('https://www.themealdb.com/api/json/v1/1/filter.php?i=' + searchBtn.value)
-      .then((res) => res.json())
-      .then((data) => {
-        showItems(data);
-        recipeDetails(data);
-    });
-}
-  
-function showItems(data) {
-    if(data.meals){
-        data.meals.forEach((food) => {
-            const foodDiv = document.createElement("div");
-            foodDiv.style.width = "25%";
-            foodDiv.innerHTML = `
-              <a href="#"  style="text-decoration: none;">
-           <img src="${food.strMealThumb}" class="card-img-top">	
-           <div class="card-body text-center bg-light ">
-             <h3 class="card-text">${food.strMeal}</h3>
-           </div>
-           </a>`;
-            document.getElementById("displayMeals").appendChild(foodDiv);
+const search = document.getElementById('search');
+search.addEventListener('click', function () {
+    const foodName = document.getElementById('searchFood').value;
+    fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${foodName}`)
+      .then(res => res.json())
+      .then(data => {
+          document.getElementById('meals').innerHTML = "";
+          document.getElementById('displayingFood').innerHTML = ' ';
+          const meals = document.getElementById('meals');
+          data.meals.forEach(element => {
+            const food = document.createElement('div')
+            food.innerHTML = `
+          <img src = "${element.strMealThumb}" onClick="handlefoodClick(${element.idMeal})">
+          <h1 onClick = "handlefoodClick(${element.idMeal})" >${element.strMeal}</h1>
+          `;
+          food.className = "card";
+          meals.appendChild(food);
         });
-        // displayMeals.classList.remove('notFound');
-    }
-    // else{
-    //     html = "Sorry, didn't match any items!";
-    //     displayMeals.classList.add('notFound');
-    // }
-    // displayMeals.innerHTML = html;
+      })
+      .catch(error => {
+        console.log(error);
+        document.getElementById('meals').innerHTML = "";
+        document.getElementById('displayingFood').innerHTML = ' ';
+        const meals = document.getElementById('meals');
+        const notFound = document.createElement('h1')
+        notFound.innerHTML = `Sorry didn't match any items`;
+        meals.appendChild(notFound);
+      })
+})
+
+let handlefoodClick = foodId => {
+  fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${foodId}`)
+    .then(res => res.json())
+    .then(data => {
+      let foodData = document.getElementById('displayingFood');
+      document.getElementById('displayingFood').innerHTML = ' ';
+      document.getElementById('displayingFood').style.display = 'block';
+      let foodDetails = document.createElement('div')
+      foodDetails.innerHTML = `
+      <img src="${ data.meals[0].strMealThumb }">
+      <h1>${ data.meals[0].strMeal }</h1>
+      <br>
+      <h1>Category: ${ data.meals[0].strCategory }</h1>
+      <br>
+      <h3> <li>${ data.meals[0].strIngredient1 }</li> </h3>
+      <h3> <li>${ data.meals[0].strIngredient2 }</li> </h3>
+      <h3> <li>${ data.meals[0].strIngredient3 }</li> </h3>
+      <h3> <li>${ data.meals[0].strIngredient4 }</li> </h3>
+      <h3> <li>${ data.meals[0].strIngredient5 }</li> </h3>
+      <h3> <li>${ data.meals[0].strIngredient6 }</li> </h3>
+      `;
+      foodDetails.className = "food-details";
+      foodData.appendChild(foodDetails);
+    })
 }
-  
-function recipeDetails(data) {
-    const addEvent = document.getElementById("displayMeals");
-    addEvent.addEventListener("click", function () {
-      document.getElementById("description").innerHTML = "";
-      let i = 1;
-      const foodName = event.target.parentNode.innerText;
-      data.meals.forEach((element) => {
-        if (element.strMeal === foodName) {
-          const foodDiv = document.createElement("div");
-          foodDiv.classList.add("col-6", "card");
-          foodDiv.innerHTML = `
-                      <img src=${element.strMealThumb} >
-                      <div class="card-body">
-                      <h3 class="card-text">${element.strMeal}</h3>
-                      <ul id="ingredients">
-                      </ul>
-                      </div>`;
-          document.getElementById("description").appendChild(foodDiv);
-          for (let i = 0; i < 10; i++) {
-            let ingredient = "strIngredient" + i++;
-            if (element[ingredient] == "") continue;
-            const newItem = document.createElement("li");
-            newItem.innerText = element[ingredient];
-            document.getElementById("ingredients").appendChild(newItem);
-          }
-          
-        }
-      });
-    });
-}
-  
